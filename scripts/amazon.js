aaf377686034e0a5a305320224cfc78c4762b062
TreeHugrrr <alaric.theophilus@gmail.com>
2025-03-13 09:32:49 +0200

    Move addToCart into cart.js

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,4 +1,4 @@
-import {cart} from '../data/cart.js'; //imports must be at the top of the file
+import {cart, addToCart} from '../data/cart.js'; //imports must be at the top of the file
 import {products} from '../data/products.js';
 
 let productsHTML = '';
@@ -59,55 +59,26 @@ products.forEach((product) => {
 
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
 
+function updateCartQuantity(productId) {
+  let cartQuantity = 0;
+
+  cart.forEach((cartItem) => {
+    cartQuantity += cartItem.quantity;
+  });
+
+  document.querySelector('.js-cart-quantity')
+    .innerHTML = cartQuantity;
+}
+
 document.querySelectorAll('.js-add-to-cart')
   .forEach((button) => {
-
-    // This solution uses a feature of JavaScript called a
-    // closure. Each time we run the loop, it will create
-    // a new variable called addedMessageTimeoutId and do
-    // button.addEventListener().
-    //
-    // Then, because of closure, the function we give to
-    // button.addEventListener() will get a unique copy
-    // of the addedMessageTimeoutId variable and it will
-    // keep this copy of the variable forever.
-    // (Reminder: closure = if a function has access to a
-    // value/variable, it will always have access to that
-    // value/variable).
-    //
-    // This allows us to create many unique copies of the
-    // addedMessageTimeoutId variable (one for every time
-    // we run the loop) so it lets us keep track of many
-    // timeoutIds (one for each product).
     let addedMessageTimeoutId;
 
     button.addEventListener('click', () => {
       const {productId} = button.dataset;
+      addToCart(productId);
+      updateCartQuantity(productId);
 
-      let matchingItem;
-      cart.forEach((item) => {
-        if (productId === item.productId) {
-          matchingItem = item;
-        }
-      });
-      const quantitySelector = document.querySelector(
-        `.js-quantity-selector-${productId}`
-      );
-      const quantity = Number(quantitySelector.value);
-      if (matchingItem) {
-        matchingItem.quantity += quantity;
-      } else {
-        cart.push({
-          productId,
-          quantity
-        });
-      }
-      let cartQuantity = 0;
-      cart.forEach((item) => {
-        cartQuantity += item.quantity;
-      });
-      document.querySelector('.js-cart-quantity')
-        .innerHTML = cartQuantity;
       const addedMessage = document.querySelector(
         `.js-added-to-cart-${productId}`
       );
